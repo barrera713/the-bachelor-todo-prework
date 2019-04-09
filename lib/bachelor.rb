ef5890f88b8f5ab905f40504ef2ea8ce57b928d8
Carlos barrera <carlosbarrera@Carloss-MacBook-Air.local>
2019-04-09 18:51:35 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
+  require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |names|
+    names.each do |key, value|
+      if value == "Winner"
+        winner_name = names["name"]
+        return winner_name.split.first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |key, value|
+    value.each do |person_data|
+      if person_data["occupation"] == occupation
+        return person_data["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  people = []
+  data.each do |key, value|
+    value.each do |person_data|
+      if person_data["hometown"] == hometown
+        people.push(key)
+      end
+    end
+  end
+  people.length
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do |person_data|
+      if person_data["hometown"] == hometown
+        return person_data["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data[season].each do |key|
+    key.each do |k, v|
+      if k == "age"
+        ages.push(v.to_f)
+      end
+    end
+  end
+  return (ages.inject(&:+)/ages.length).round
 end
